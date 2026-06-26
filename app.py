f590c252233ecec186bec5f202816945121fa281
Jesus <jackedwithjesus@gmail.com>
2026-06-26 13:59:13 -0700

    Load local environment variables

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import random
 import requests
 import sys
 import csv
+from dotenv import load_dotenv
+
+load_dotenv()
+
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(levelname)s %(name)s: %(message)s",
                     stream=sys.stdout)
